a38c38950e20f4cee7e3526612ca3e93fa8a4594
Collins Mutai <collinsfrontend@gmail.com>
2023-04-28 21:51:19 +0300

    initial

---
+++ routes/admin.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const path = require("path");
+
+const rootDir = require("../util/path");
+
+const router = express.Router();
+
+router.get("/add-product", (req, res, next) => {
+  //   res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+});
+router.post("/add-product", (req, res, next) => {
+    console.log(req.body);
+  res.redirect("/");
+  //   res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
+});
+
+module.exports = router;
