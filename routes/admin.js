362de81ee23547026c20faf5a039ac96d9381c17
Collins Mutai <collinsfrontend@gmail.com>
2023-05-20 11:39:13 +0300

    add cart model

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,24 @@
-const express = require("express");
-
 const path = require("path");
 
-const rootDir = require("../util/path");
+const express = require("express");
+
+const adminController = require("../controllers/admin");
 
 const router = express.Router();
 
-router.get("/add-product", (req, res, next) => {
-  //   res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
-});
-router.post("/add-product", (req, res, next) => {
-    console.log(req.body);
-  res.redirect("/");
-  //   res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
-});
+// /admin/add-product => GET
+router.get("/add-product", adminController.getAddProduct);
+
+// /admin/products => GET
+router.get("/products", adminController.getProducts);
+
+// /admin/add-product => POST
+router.post("/add-product", adminController.postAddProduct);
+
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
+router.post("/edit-product", adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct)
 
 module.exports = router;
